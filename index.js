eb0893d9235bf43bbac95a9b6af7cef6d5c702a4
Andre-Jacques de Beer <a.j.debeer@gmail.com>
2018-08-19 17:08:39 -0400

    deconstructed monolith to revealing module pattern

--- index.js
+++ index.js
@@ -17,113 +17,138 @@ const zplify = (function() {
       return columnRatios.map(proportion => Math.floor(proportion*tableWidth));
     }
     return columns.map(col => Math.floor(tableWidth/columns.length));
-  }
+  };
 
-  return {
-    generateTable: function({ rows = [], config = {} }) {
+  const createBorderBox = (labelPadding, tableHeight, tableWidth, borderWidth) => {
+    return '^FO'+labelPadding
+           +','+labelPadding
+           +'^GB'+tableWidth
+           +','+tableHeight
+           +','+borderWidth
+           +'^FS\n';
+  };
 
-      const columns = rows[0]; // 
-
-      const {
-        labelWidth: labelWidthInches = 4, // inches
-        labelHeight: labelHeightInches = 6, // inches
-        printDensity = 203, // dots per inch (dpi)
-        labelPadding = 50, // dots
-        borderWidth = 3, // dots
-        columnRatios = [], // array of decimal ratios that must add up to 1 - uniform by default
-        fontSize = 30, // dots
-        maxRowHeight = 2*fontSize, // dots
-        rowHeight = maxRowHeight, // dots
-        cellPadding = 20 // dots
-      } = config;
-
-      const labelWidth = labelWidthInches*printDensity;
-      const labelHeight = labelHeightInches*printDensity;
-
-      const tableWidth = labelWidth - 2*labelPadding;
-      const tableHeight = rows.length*rowHeight;
-
-      const columnWidths = getColumnWidths(columnRatios, columns, tableWidth);
-
-      // there are 6 steps
-
-      // 1. Create opening
-      let zplTable = '^XA\n';
-
-      // 2. Create border box
-      zplTable += '^FO'+labelPadding
+  const createVerticalSeparators = (columns, columnWidths, labelPadding, tableHeight, borderWidth) => {
+    let runningSum = 0;
+    let separators = '';
+    columns.forEach((column, index) => {
+      if (index === columns.length-1) return;
+      runningSum += columnWidths[index];
+      separators += '^FO'+(labelPadding+runningSum)
                +','+labelPadding
-               +'^GB'+tableWidth
-               +','+tableHeight
+               +'^GB1,'+tableHeight
                +','+borderWidth
                +'^FS\n';
+    });
+    return separators;
+  };
 
-      // 3. Create vertical separators
-      let runningSum = 0;
-      columns.forEach((column, index) => {
-        if (index === columns.length-1) return;
-        runningSum += columnWidths[index];
-        zplTable += '^FO'+(labelPadding+runningSum)
-                 +','+labelPadding
-                 +'^GB1,'+tableHeight
-                 +','+borderWidth
-                 +'^FS\n';
+  const createHorizontalSeparators = (rows, rowHeight, labelPadding, tableWidth, borderWidth) => {
+    let runningSum = 0;
+    let separators = '';
+    rows.forEach((row, index) => {
+      if (index === rows.length-1) return;
+      runningSum += rowHeight;
+      separators += '^FO'+labelPadding
+               +','+(labelPadding+runningSum)
+               +'^GB'+tableWidth
+               +',1'
+               +','+borderWidth
+               +'^FS\n';
+    });
+    return separators;
+  };
+
+  const createAllCellContent = (fontSize, labelPadding, rowHeight, columns, rows, cellPadding, columnWidths) => {
+    const cells = [];
+    cells.push('^CF0,'+fontSize+','+fontSize);
+    let runningYSum = labelPadding;
+    rows.forEach((row, rowIndex) => {
+      const yOffset = runningYSum+(rowHeight-fontSize)/1.5;
+      runningYSum += rowHeight;
+
+      let runningXSum = labelPadding;
+      columns.forEach((col, colIndex) => {
+        let xOffset = runningXSum;
+        if (rowIndex !== 0) {
+          xOffset += cellPadding;
+        }
+        runningXSum += columnWidths[colIndex];
+        let cell = '^FO'+xOffset+','+yOffset;
+        if (rowIndex === 0) {
+          cell += '^A0N,'
+               +fontSize
+               +','+(fontSize+2)
+               +'^FB'+columnWidths[colIndex]
+               +',1,0,C^FD';
+        }
+        else {
+          cell += '^FD';
+        }
+        cell += (typeof row[colIndex] === 'undefined' ? '' : row[colIndex])+'^FS';
+        cells.push(cell);
       });
+    });
 
-      // 4. Create horizontal separators
-      runningSum = 0;
-      rows.forEach((row, index) => {
-        if (index === rows.length-1) return;
-        runningSum += rowHeight;
-        zplTable += '^FO'+labelPadding
-                 +','+(labelPadding+runningSum)
-                 +'^GB'+tableWidth
-                 +',1'
-                 +','+borderWidth
-                 +'^FS\n';
-      });
+    return cells.join('\n') + '\n';
+  };
 
-      // 5. Fill in content
-      const cells = [];
-      cells.push('^CF0,'+fontSize+','+fontSize);
-      let runningYSum = labelPadding;
-      rows.forEach((row, rowIndex) => {
-        const yOffset = runningYSum+(rowHeight-fontSize)/1.5;
-        runningYSum += rowHeight;
+  const generateTable = ({ rows = [], config = {} }) => {
+    const columns = rows[0]; // 
 
-        let runningXSum = labelPadding;
-        columns.forEach((col, colIndex) => {
-          let xOffset = runningXSum;
-          if (rowIndex !== 0) {
-            xOffset += cellPadding;
-          }
-          runningXSum += columnWidths[colIndex];
-          let cell = '^FO'+xOffset+','+yOffset;
-          if (rowIndex === 0) {
-            cell += '^A0N,'
-                 +fontSize
-                 +','+(fontSize+2)
-                 +'^FB'+columnWidths[colIndex]
-                 +',1,0,C^FD';
-          }
-          else {
-            cell += '^FD';
-          }
-          cell += (typeof row[colIndex] === 'undefined' ? '' : row[colIndex])+'^FS';
-          cells.push(cell);
-        });
-      });
+    const {
+      labelWidth: labelWidthInches = 4, // inches
+      labelHeight: labelHeightInches = 6, // inches
+      printDensity = 203, // dots per inch (dpi)
+      labelPadding = 50, // dots
+      borderWidth = 3, // dots
+      columnRatios = [], // array of decimal ratios that must add up to 1 - uniform by default
+      fontSize = 30, // dots
+      maxRowHeight = 2*fontSize, // dots
+      rowHeight = maxRowHeight, // dots
+      cellPadding = 20 // dots
+    } = config;
 
-      zplTable += cells.join('\n') + '\n';
+    const labelWidth = labelWidthInches*printDensity;
+    const labelHeight = labelHeightInches*printDensity;
 
-      // 6. Create closing
-      zplTable += '^XZ';
+    const tableWidth = labelWidth - 2*labelPadding;
+    const tableHeight = rows.length*rowHeight;
 
+    const columnWidths = getColumnWidths(columnRatios, columns, tableWidth);
 
-      return zplTable;
+    // there are 6 steps
 
-    }
-  }
+    // 1. Create opening
+    let zplTable = '^XA\n';
+
+    // 2. Create border box
+    zplTable += createBorderBox(labelPadding, tableHeight, tableWidth, borderWidth);
+
+    // 3. Create vertical separators
+    zplTable += createVerticalSeparators(columns, columnWidths, labelPadding, tableHeight, borderWidth);
+
+    // 4. Create horizontal separators
+    zplTable += createHorizontalSeparators(rows, rowHeight, labelPadding, tableWidth, borderWidth);
+
+    // 5. Fill in content
+    zplTable += createAllCellContent(fontSize, labelPadding, rowHeight, columns, rows, cellPadding, columnWidths);
+
+    // 6. Create closing
+    zplTable += '^XZ';
+
+    return zplTable;
+  };
+
+  return {
+    getArraySum,
+    getColumnWidths,
+    createBorderBox,
+    createVerticalSeparators,
+    createHorizontalSeparators,
+    createAllCellContent,
+    generateTable
+  };
 })();
 
 module.exports = zplify;
